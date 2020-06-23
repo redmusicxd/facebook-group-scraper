42173fcbe4abdf158dba7caff7de0dd07dcd6d78
Dan Negru <rednblka@gmx.com>
2020-06-23 21:04:13 +0300

    KIll the active session if command received Schedule the job properly Get data from last run on Socket.io connection event

--- index.js
+++ index.js
@@ -8,65 +8,82 @@ const server = require('http').createServer(app);
 server.listen(8443);
 const io = require('socket.io')(server);
 const {scrollWhileChanging} = require('./scroll');
-
+var posts = {};
+(async () => {
+var browser;
+var page;
 io.on('connection',(socket) => {
-    socket.on('command',() =>{
+    socket.emit("init", posts);
+    socket.on('command', async () =>{
+        if(browser && browser.isConnected()){
+            await page.close();
+            await browser.close();
+        }
+        browser = await puppeteer.launch({headless: true});
+        page = await browser.newPage();
         start();
         console.log("received");
-        
     })
 })
-start()
-sched.scheduleJob('* 18 * * *', () => {
+sched.scheduleJob('* 17 * * *', () => {
     start();
 })
-
+function scrollend(){
+    io.emit("news", posts);
+    console.log(posts);
+}
 async function start() {
     const today = new Date().getDate();
     var found = 0;
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    await page.goto(baseurl);
+    posts = {};
+    try {
+        await page.goto(baseurl);
+    } catch (error) {
+        console.log("Browser disconnected");
+        
+    }
     
-    await page.exposeFunction('getItem', function(date, link) {
-        var parsed = () => {if((parser(parser(link, true).query.u).host === "static.moonactive.net") || parser(parser(link, true).query.u).host === "coinmaster.onelink.me") {return parser(parser(link, true).query.u, true).href}};
-        if(parsed() && (today - new Date(date * 1000).getDate()) < 3){
-            found++;
-            io.emit("news",{date: new Date(date * 1000).toISOString(), link: parsed()});
-            // console.log('send')
-            console.log(found);
-        }
-    });
-
-    // const posts = await page.$$('.userContent a');
-
-    // for (let i = 0; i < posts.length; i++) {
-    //     const post = await (await posts[i].getProperty('href')).jsonValue();
-    //     var parsed = () => {if((parser(parser(post, true).query.u).host == "static.moonactive.net") || parser(parser(post, true).query.u).host == "coinmaster.onelink.me") return parser(parser(post, true).query.u,true).href};
-    //     console.log(parsed());
-    //   }
-    
-    await page.evaluate(() => {
-        for(var i=0;i<=document.querySelectorAll('.userContent a').length - 1;i++){
-            if(document.querySelectorAll('.userContent a')[i].parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr") != null)
-                getItem(document.querySelectorAll('.userContent a')[i].parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr").dataset.utime ,document.querySelectorAll('.userContent a')[i].href)
-        }
-        var observer = new MutationObserver((mutations) => { 
-            for(var mutation of mutations) {
-                if(mutation.addedNodes.length) {
-                    for(var i=0;i<=mutation.addedNodes.length;i++){
-                        if((mutation.addedNodes[i] != (undefined || null) ) && (mutation.addedNodes[i].id != (undefined || null)) && (document.getElementById(mutation.addedNodes[i].id) != null) && a.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr") != null){
-                            document.getElementById(mutation.addedNodes[i].id).querySelectorAll('.userContent a').forEach(a => {
-                                getItem(a.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr").dataset.utime, a.href);
-                            })
+    try {
+        await page.exposeFunction('getItem', function(date, link) {
+            var parsed = () => {if((parser(parser(link, true).query.u).host === "static.moonactive.net") || parser(parser(link, true).query.u).host === "coinmaster.onelink.me") {return parser(parser(link, true).query.u, true).href}};
+            if(parsed() && (today - new Date(date * 1000).getDate()) < 3){
+                found++;
+                posts[new Date(date * 1000).toISOString() + "_" + found] = parsed();
+            }
+        });
+    } catch (error) {
+        console.log('session disposed');
+    }
+    try {
+        await page.evaluate(() => {
+            for(var i=0;i<=document.querySelectorAll('.userContent a').length - 1;i++){
+                if(document.querySelectorAll('.userContent a')[i].parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr") != null)
+                    getItem(document.querySelectorAll('.userContent a')[i].parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr").dataset.utime ,document.querySelectorAll('.userContent a')[i].href)
+            }
+            var observer = new MutationObserver((mutations) => { 
+                for(var mutation of mutations) {
+                    if(mutation.addedNodes.length) {
+                        for(var i=0;i<=mutation.addedNodes.length;i++){
+                            if((mutation.addedNodes[i] != (undefined || null) ) && (mutation.addedNodes[i].id != (undefined || null)) && (document.getElementById(mutation.addedNodes[i].id) != null) && a.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr") != null){
+                                document.getElementById(mutation.addedNodes[i].id).querySelectorAll('.userContent a').forEach(a => {
+                                    getItem(a.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("abbr").dataset.utime, a.href);
+                                })
+                            }
                         }
                     }
                 }
-            }
+            });
+            observer.observe(document.getElementById('group_mall_211760099500451'), { attributes: false, childList: true, subtree: true });
         });
-        observer.observe(document.getElementById('group_mall_211760099500451'), { attributes: false, childList: true, subtree: true });
-    });
+        
+    } catch (error) {
+        console.log("again the page is disposed");
+        
+    }
     await scrollWhileChanging(page, 10);
-    
-    await browser.close();
+
+    await browser.close().then(() => {
+        scrollend();
+    });
 };
+})();
